c4fde65e7f16433d08bca013152033facfe15b21
AkhmadRamadani <ar20011118@gmail.com>
2020-08-18 12:23:20 +0700

    init commit

---
+++ dewiSri/build/precache-manifest.6e6e4188a2bdf43d7eb82faf4f877a94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3da276a070253ca35668d595c8459e2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c41cd773ab5afdf867ce",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8bc8382dfac2f8e7365d",
+    "url": "/static/js/2.76eb7f63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.76eb7f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c41cd773ab5afdf867ce",
+    "url": "/static/js/main.c43647a8.chunk.js"
+  },
+  {
+    "revision": "1e484e1f3773af645d34",
+    "url": "/static/js/runtime-main.44387846.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
